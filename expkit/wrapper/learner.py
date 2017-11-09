128b85c9bb3fa5ff3b4486c9e783e3fa1010b01c
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2017-11-09 11:32:04 -0500

    adding events

--- expkit/wrapper/learner.py
+++ expkit/wrapper/learner.py
@@ -1,13 +1,48 @@
 from .operators import unwrapped
 
 
+class Event(object):
+    def __init__(self, callee, *args, **kwargs):
+        self.callee = callee
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, emitter):
+        return self.callee(emitter, *self.args, **self.kwargs)
+
+
+class EventRegistry(object):
+    def __init__(self, emitter):
+        self.emitter = emitter
+        self.events = {}
+
+
+    def register_event(self, name, event):
+        if name in self.events:
+            self.events[name].append(event)
+        else:
+            self.events.update({name: [event]})
+
+
+    def emit(self, name):
+        if name in self.events.keys():
+            return tuple(map(lambda event: event(self.emitter), self.events[name]))
+        else:
+            return tuple()
+
+
 class LearnerWrapper(object):
     def __init__(self, estimator_class, *args, **kwargs):
         self.estimator_class = estimator_class
         self.args = args
         self.kwargs = kwargs
+        self.events = EventRegistry(self)
 
         self.wrapped = None
+
+
+    def register_event(self, name, event):
+        self.events.register_event(name, event)
 
 
     def __is_wrapper__(self):
@@ -15,15 +50,20 @@ class LearnerWrapper(object):
 
 
     def __wrapped__(self, *args, **kwargs):
+        self.events.emit("unwrap")
         if not self.wrapped:
+            self.events.emit("estimator_instantiation")
             self.wrapped = self.estimator_class(*args, *self.args, **kwargs, **self.kwargs)
         return self.wrapped
 
 
     def fit(self, X, y):
-        return unwrapped(self).fit(X, y)
+        learner = unwrapped(self)
+        self.events.emit("fit")
+        return learner.fit(X, y)
 
 
     def predict(self, X):
+        self.events.emit("predict")
         return unwrapped(self).predict(X)
 
