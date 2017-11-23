1cc45fc547701fe2f195567d6df6dd834df14893
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2017-11-23 16:48:38 -0500

    rechanging labels_to_onehots shape + changing formatting of datetime

--- expkit/format/time.py
+++ expkit/format/time.py
@@ -1,4 +1,7 @@
-from time import gmtime, strftime
+from time import gmtime, strftime, mktime
+import calendar
+from datetime import datetime
+from dateutil import tz
 
 
 class Time(object):
@@ -8,3 +11,7 @@ class Time(object):
 
     def __str__(self):
         return strftime("%Y-%m-%d_%H.%M.%S", self.utc_time_t)
+
+
+    def __repr__(self):
+        return str(datetime.fromtimestamp(calendar.timegm(self.utc_time_t)))
