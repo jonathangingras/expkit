744026142baed70102451a0647f63d134f1ce991
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2018-03-28 14:32:44 -0400

    fix bug graphics ipython

--- expkit/graphics/ipython_matplotlib.py
+++ expkit/graphics/ipython_matplotlib.py
@@ -11,11 +11,14 @@ def show(pyplot_code, magic_arg="inline", fallback_filename=None, save_anyways=F
         if fallback_filename:
             save_to_pdf(fallback_filename, pyplot_code, *args, **kwargs)
 
+    already_saved = False
+
     if magic_available():
         get_ipython().magic("matplotlib {}".format(magic_arg))
         pyplot_code(*args, **kwargs)
-        if save_anyways:
-            save_pdf()
-            return
     else:
+        save_pdf()
+        already_saved = True
+
+    if save_anyways and not already_saved:
         save_pdf()
